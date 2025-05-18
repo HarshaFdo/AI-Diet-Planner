d393a705f291b2600b0496674623c21bf32aa19b
HarshaFdo <fernando.harsha2016@gmail.com>
2025-05-18 10:26:13 +0530

    styling home screen 1

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import React, { useContext, useEffect } from "react";
 import { UserContext } from "./../../context/UserContext";
 import { router } from "expo-router";
+import HomeHeader from "../../components/shared/HomeHeader";
+import TodayProgress from "../../components/shared/TodayProgress";
 
 export default function Home() {
   const { user } = useContext(UserContext);
@@ -13,8 +15,13 @@ export default function Home() {
   }, [user]);
 
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        padding: 20,
+      }}
+    >
+      <HomeHeader />
+      <TodayProgress />
     </View>
   );
 }
---
+++ components/shared/HomeHeader.jsx
@@ -0,0 +1,44 @@
+import { View, Text, Image } from "react-native";
+import React, { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+
+export default function HomeHeader() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+      }}
+    >
+      <Image
+        source={require("./../../assets/images/user.png")}
+        style={{
+          width: 60,
+          height: 60,
+          borderRadius: 99,
+        }}
+      />
+      <View>
+        <Text
+          style={{
+            fontSize: 20,
+          }}
+        >
+          Hello👋,
+        </Text>
+        <Text
+          style={{
+            fontSize: 23,
+            fontWeight: "bold",
+          }}
+        >
+          {user?.name}
+        </Text>
+      </View>
+    </View>
+  );
+}
